e293a396c2c88c0e5719c6fceb2a46ea3b54fd1a
dryvenn <dryvenn@gmail.com>
2020-06-21 16:49:55 +0100

    Add a configmap manager

---
+++ configmaps.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	core_v1 "k8s.io/api/core/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	watch "k8s.io/apimachinery/pkg/watch"
+	k8s "k8s.io/client-go/kubernetes"
+)
+
+// ConfigMapManager is a simple wrapper around the Kubernetes API
+// that provides app-specific helpers from on base primitives.
+type ConfigMapManager struct {
+	clientset   k8s.Interface
+	watchHandle watch.Interface
+}
+
+// NewConfigMapManager creates a new ConfigMapManager
+func NewConfigMapManager(clientset k8s.Interface) *ConfigMapManager {
+	return &ConfigMapManager{
+		clientset: clientset,
+	}
+}
+
+// StartWatching returns a channel in which ConfigMap objects will be sent
+// whenever a ConfigMap is added or changed in the cluster.
+// It will ignore events from errors and deletions.
+func (m *ConfigMapManager) StartWatching() (<-chan *core_v1.ConfigMap, error) {
+	m.StopWatching()
+
+	handle, err := m.clientset.CoreV1().ConfigMaps("").Watch(meta_v1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	m.watchHandle = handle
+
+	// TODO: determine the correct buffering here
+	configMaps := make(chan *core_v1.ConfigMap, 100)
+
+	go m.processEvents(handle.ResultChan(), configMaps)
+
+	return configMaps, nil
+}
+
+// StopWatching triggers cancellation of ConfigMap watching, eventually leading
+// to the closure of the channel returned by StartWatching().
+func (m *ConfigMapManager) StopWatching() {
+	if m.watchHandle == nil {
+		return
+	}
+	m.watchHandle.Stop()
+	m.watchHandle = nil
+}
+
+// processEvents transforms event objects into configmap objects, and filters
+// events by type.
+func (m *ConfigMapManager) processEvents(events <-chan watch.Event, configMaps chan<- *core_v1.ConfigMap) {
+EventLoop:
+	for event := range events {
+		// Only keep mutations, ignore deletions and errors
+		switch event.Type {
+		case watch.Added, watch.Modified:
+		default:
+			continue EventLoop
+		}
+		if cm, ok := event.Object.(*core_v1.ConfigMap); ok {
+			configMaps <- cm
+		}
+	}
+	close(configMaps)
+}
---
+++ configmaps_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	core_v1 "k8s.io/api/core/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+	"testing"
+	"time"
+)
+
+func testChanEmpty(t *testing.T, c chan interface{}) {
+	// Sleep a bit for propagation to happen
+	time.Sleep(100 * time.Millisecond)
+	if len(c) != 0 {
+		t.Error("chan not empty")
+	}
+}
+
+func TestConfigMapWatching(t *testing.T) {
+	clientset := fake.NewSimpleClientset()
+	configMapClient := clientset.CoreV1().ConfigMaps("")
+
+	manager := NewConfigMapManager(clientset)
+	notifChan, err := manager.StartWatching()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testNoNotif := func() {
+		time.Sleep(100 * time.Millisecond)
+		if len(notifChan) != 0 {
+			t.Error("configmap chan not empty")
+		}
+	}
+
+	// create 1 configmap
+	testConfigMap := core_v1.ConfigMap{
+		ObjectMeta: meta_v1.ObjectMeta{
+			Name: "test",
+		},
+	}
+	configMapClient.Create(&testConfigMap)
+
+	// receive one configmap
+	notif := <-notifChan
+	if notif.Name != testConfigMap.Name {
+		t.Fatal("configmap name mismatch")
+	}
+	testNoNotif()
+
+	// change configmap
+	testConfigMap.Data = map[string]string{"key": "val"}
+	configMapClient.Update(&testConfigMap)
+
+	// receive one configmap
+	notif = <-notifChan
+	if notif.Name != testConfigMap.Name {
+		t.Fatal("configmap name mismatch")
+	}
+	if notif.Data["key"] != testConfigMap.Data["key"] {
+		t.Fatal("configmap val mismatch")
+	}
+	testNoNotif()
+
+	// delete configmap
+	configMapClient.Delete(testConfigMap.Name, &meta_v1.DeleteOptions{})
+	testNoNotif()
+
+	manager.StopWatching()
+	testNoNotif()
+	_, ok := <-notifChan
+	if ok {
+		t.Error("configmap chan not closed")
+	}
+}
