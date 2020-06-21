0f321061e65a2eb283e9165f1572ab1a266e0595
dryvenn <dryvenn@gmail.com>
2020-06-21 16:02:12 +0100

    Add a simple page fetcher

---
+++ fetcher.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+// PageFetcher is an unordered set of keyword-URL couples.
+type PageFetcher map[string]string
+
+// FetchError is a collection of errors that happened during Fetch().
+type FetchError map[string]error
+
+func (e FetchError) Error() string {
+	if e == nil {
+		return ""
+	}
+	ret := make([]string, 0, len(e))
+	for key, err := range e {
+		ret = append(ret, fmt.Sprintf("%s='%v'", key, err))
+	}
+	return strings.Join(ret, " ")
+}
+
+// Fetch will HTTP-Get the content of each keyword's URL and return it.
+// It will return a non-nil error when at least one the call failed.
+func (pf PageFetcher) Fetch() (map[string]string, error) {
+	ret := make(map[string]string)
+	errs := make(FetchError)
+	for key, url := range pf {
+		res, err := http.Get(url)
+		if err != nil {
+			errs[key] = err
+			continue
+		}
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			errs[key] = err
+			continue
+		}
+		ret[key] = string(body)
+	}
+	if len(errs) != 0 {
+		return ret, errs
+	}
+	// Make sure a nil type is returned when there is no error.
+	return ret, nil
+}
+
+// PageFetcherFromString returns a new PageFetcher built from a space-separated
+// list of key=url couples.
+// It shall return an error if any couple is not parsable.
+func PageFetcherFromString(s string) (PageFetcher, error) {
+	pf := make(PageFetcher)
+
+	for _, couple := range strings.Split(s, " ") {
+		if len(couple) == 0 {
+			continue
+		}
+		keyval := strings.SplitN(couple, "=", 2)
+		if len(keyval) != 2 {
+			return pf, fmt.Errorf("cannot parse '%s' as key=value", couple)
+		}
+		pf[keyval[0]] = keyval[1]
+	}
+
+	return pf, nil
+}
---
+++ fetcher_test.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestFetch(t *testing.T) {
+	tests := []struct {
+		name    string
+		fetcher PageFetcher
+		// map of key/page-substring
+		success map[string]string
+	}{
+		{
+			name: "simple good",
+			fetcher: PageFetcher{
+				"google": "https://google.com",
+			},
+			success: map[string]string{"google": "google"},
+		},
+		{
+			name: "simple bad",
+			fetcher: PageFetcher{
+				"gogole": "https://gogole.com",
+			},
+			success: map[string]string{},
+		},
+		{
+			name: "simple multi",
+			fetcher: PageFetcher{
+				"google": "https://google.com",
+				"gogole": "https://gogole.com",
+			},
+			success: map[string]string{"google": "google"},
+		},
+		{
+			name: "multi",
+			fetcher: PageFetcher{
+				"google":      "https://google.com",
+				"datanet":     "http://data.net",
+				"datanets":    "https://data.net",
+				"cloudflare":  "http://1.1.1.1",
+				"cloudflares": "https://1.1.1.1",
+
+				"something else": "invalid url",
+			},
+			success: map[string]string{
+				"google":      "google",
+				"datanet":     "Now, now, we've all seen a web server before.",
+				"datanets":    "Now, now, we've all seen a web server before.",
+				"cloudflare":  "makes your Internet faster",
+				"cloudflares": "makes your Internet faster",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, err := test.fetcher.Fetch()
+			// Each positive result is a key-page couple.
+			// They key must be in 'success', and the page must contain the 'success'
+			// substring.
+			for key, page := range res {
+				if _, ok := test.fetcher[key]; !ok {
+					t.Errorf("unknown key %s", key)
+				}
+				if sub, ok := test.success[key]; !ok {
+					t.Errorf("%s key not in success", key)
+				} else if !strings.Contains(page, sub) {
+					t.Errorf("'%s' not in %s page", sub, key)
+				}
+			}
+			// Test all positive results are accounted for.
+			if len(test.success) != len(res) {
+				t.Error("missing success results")
+			}
+			// Test error is correctly nil if applicable.
+			if len(test.fetcher)-len(test.success) == 0 {
+				if err != nil {
+					t.Fatal("expected nil error")
+				}
+				return
+			}
+			fetchErr, ok := err.(FetchError)
+			if !ok {
+				t.Fatalf("%v is not a FetchError", err)
+			}
+			// Each negative result is a key-error couple.
+			// They key must not be in 'success'.
+			for key, _ := range fetchErr {
+				if _, ok := test.fetcher[key]; !ok {
+					t.Errorf("unknown key %s", key)
+				}
+				if _, ok := test.success[key]; ok {
+					t.Errorf("%s key in success", key)
+				}
+			}
+		})
+	}
+}
+
+func TestPageFetcherFromString(t *testing.T) {
+	tests := []struct {
+		input   string
+		output  PageFetcher
+		success bool
+	}{
+		{
+			input:   "key=val",
+			output:  PageFetcher{"key": "val"},
+			success: true,
+		},
+		{
+			input:   "key1=val1 key2=val2",
+			output:  PageFetcher{"key1": "val1", "key2": "val2"},
+			success: true,
+		},
+		{
+			input:   "key1=val1  key2=val2",
+			output:  PageFetcher{"key1": "val1", "key2": "val2"},
+			success: true,
+		},
+		{
+			input:   "key1",
+			output:  PageFetcher{},
+			success: false,
+		},
+		{
+			input:   "key1 key2",
+			output:  PageFetcher{},
+			success: false,
+		},
+		{
+			input:   "key1 = key2",
+			output:  PageFetcher{},
+			success: false,
+		},
+		{
+			input:   "key1=val1 key2",
+			output:  PageFetcher{"key1": "val1"},
+			success: false,
+		},
+		{
+			input:   "key1=val1=val11 key2=val2",
+			output:  PageFetcher{"key1": "val1=val11", "key2": "val2"},
+			success: true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			res, err := PageFetcherFromString(test.input)
+			if test.success && err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if len(res) != len(test.output) {
+				t.Fatalf("expected %d couples, got %d %v", len(test.output), len(res), res)
+			}
+			for k, v := range res {
+				if out, ok := test.output[k]; !ok {
+					t.Errorf("unexpected key %s", k)
+				} else if out != v {
+					t.Errorf("unexpected val %s", v)
+				}
+			}
+		})
+	}
+}
